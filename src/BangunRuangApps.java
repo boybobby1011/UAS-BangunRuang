9a85128f3798ac764f93855c8dfb5b7d3110d555
_boybobby <138386602+boybobby1011@users.noreply.github.com>
2025-01-22 21:22:55 +0700

    Update BangunRuangApps.java

--- src/BangunRuangApps.java
+++ src/BangunRuangApps.java
@@ -5,6 +5,7 @@ public class BangunRuangApps {
         Scanner scanner = new Scanner(System.in);
         BangunRuang bangunRuang = new BangunRuang();
 
+        // Input untuk balok
         System.out.println("=== Hitung Balok ===");
         System.out.print("Masukkan panjang: ");
         bangunRuang.setPanjang(scanner.nextInt());
@@ -13,16 +14,20 @@ public class BangunRuangApps {
         System.out.print("Masukkan tinggi: ");
         bangunRuang.setTinggi(scanner.nextInt());
 
+        // Menampilkan hasil perhitungan balok
         System.out.println("Luas Permukaan Balok: " + bangunRuang.luasBalok());
         System.out.println("Volume Balok: " + bangunRuang.volumeBalok());
 
+        // Input untuk bola
         System.out.println("\n=== Hitung Bola ===");
         System.out.print("Masukkan diameter: ");
         bangunRuang.setDiameter(scanner.nextInt());
 
+        // Menampilkan hasil perhitungan bola
         System.out.println("Luas Permukaan Bola: " + bangunRuang.luasBola());
         System.out.println("Volume Bola: " + bangunRuang.volumeBola());
 
         scanner.close();
     }
 }
+
